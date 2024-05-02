65f7bd0a708bf4ab3050d755e45e626f1a9d12c7
Bruno Braga <brunobragaw8t@gmail.com>
2024-05-02 21:22:45 +0100

    feature: enable Nuxt layouts

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-screen">
+    <div class="flex-1 p-4">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is index.vue</h1>
+  </div>
+</template>
